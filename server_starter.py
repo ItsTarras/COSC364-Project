515c0c06bf165341e43ed4b54984f92aeef8191a
Max Croucher <mcr93@uclive.ac.nz>
2023-04-13 11:52:47 +1200

    added fixed set of testing configs

--- server_starter.py
+++ server_starter.py
@@ -7,6 +7,7 @@ config_builder.NUM_ROUTERS = 8
 ROUTER_FILENAME = "rip_router.py"
 CONFIG_PREFIX = f"{config_builder.CONFIG_LOCATION}/config_"
 CONFIG_SUFFIX = ".txt"
+TESTING_PREFIX = "demo_config/"
 
 def run_router(router_id):
     os.system(f"gnome-terminal -- bash -c \"python3 {ROUTER_FILENAME} {CONFIG_PREFIX}{router_id}{CONFIG_SUFFIX}; bash\" ")
@@ -20,5 +21,18 @@ def main():
     for i in processes:
         i.start()
 
+def run_test_router(router_id):
+    os.system(f"gnome-terminal -- bash -c \"python3 {ROUTER_FILENAME} {TESTING_PREFIX}{router_id}{CONFIG_SUFFIX}; bash\" ")
+
+
+def run_test_configs():
+    id_nums = ["05", "15", "25", "35", "45", "55"]
+    processes = []
+    for router_id in id_nums:
+        processes.append(multiprocessing.Process(target=run_test_router, args=(router_id, )))
+    
+    for i in processes:
+        i.start()
 if __name__ == "__main__":
-    main()
+    #main()
+    run_test_configs()
